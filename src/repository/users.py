6af58c5577814bb30278d9cbab0a951f7d65ad5f
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-04-28 15:15:20 +0300

    Update users.py

--- src/repository/users.py
+++ src/repository/users.py
@@ -2,7 +2,7 @@ from libgravatar import Gravatar
 from sqlalchemy.orm import Session
 
 from src.database.models import User
-from src.schemas.schemas import UserModel
+from src.schemas.schemas_auth import UserModel
 
 
 async def get_user_by_email(email: str, db: Session) -> User:
@@ -10,6 +10,19 @@ async def get_user_by_email(email: str, db: Session) -> User:
 
 
 async def create_user(body: UserModel, db: Session) -> User:
+    """
+    The create_user function creates a new user in the database.
+        Args:
+            body (UserModel): The UserModel object containing the data to be inserted into the database.
+            db (Session): The SQLAlchemy Session object used to interact with the database.
+        Returns:
+            User: A newly created user from the database.
+    
+    :param body: UserModel: Create a new user
+    :param db: Session: Create a database session
+    :return: A user object
+    :doc-author: Trelent
+    """
     avatar = None
     try:
         g = Gravatar(body.email)
@@ -24,5 +37,14 @@ async def create_user(body: UserModel, db: Session) -> User:
 
 
 async def update_token(user: User, token: str | None, db: Session) -> None:
+    """
+    The update_token function updates the refresh token for a user.
+    
+    :param user: User: Identify the user that will have their token updated
+    :param token: str | None: Update the user's refresh token
+    :param db: Session: Commit the changes to the database
+    :return: None
+    :doc-author: Trelent
+    """
     user.refresh_token = token
     db.commit()
