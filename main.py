81e2865030621fe65bbef20487f43d067a71b61a
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-04-28 15:07:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,36 @@ import re
 from pathlib import Path
 
 from fastapi import FastAPI, Depends, HTTPException, Request, status
-from src.services.auth import Auth # Імпорт класу Auth та класу UserRole з auth.py
+from src.services.auth import Auth 
 from src.database.models import User, Role
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.database.db import get_db
+
+from src.routes import auth
+
 import uvicorn
 
 app = FastAPI()
 
 
 
+app.include_router(auth.router, prefix="/api")
+
 # Створимо екземпляр класу Auth для використання
 auth_service = Auth()
 
-@app.get("/")
-def index():
-    return {'message': 'PhotoShare Application'}
 
-# Додамо приклад використання класу Auth
-@app.get("/protected-route")
-async def protected_route(current_user: User = Depends(auth_service.get_current_user(role=Role.ADMIN))):
-    return {"message": "Welcome to the protected route, Admin!"}
+@app.get("/")
+async def home_page(request: Request, db: AsyncSession = Depends(get_db)):
+    #Створення ролей для юзерів
+    roles = ["admin", "moderator", "user"]
+    for current_users in roles:
+        existing_role = db.query(Role).filter(Role.role == current_users).first()
+        if not existing_role:
+            db.add(Role(role=current_users))
+    db.commit()
+    
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
