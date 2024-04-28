1154c7630f69ee2d16ddae08c63346ad5d814077
DajanaD <ccv080480@gmail.com>
2024-04-28 13:28:03 +0300

    Authentication

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ import re
 from pathlib import Path
 
 from fastapi import FastAPI, Depends, HTTPException, Request, status
-#from sqlalchemy.ext.asyncio import AsyncSession
-#from src.database.db import get_db
+from src.services.auth import Auth, UserRole  # Імпорт класу Auth та класу UserRole з auth.py
+from src.database.models import User
 import uvicorn
-
 
 app = FastAPI()
 
 
 
-
+# Створимо екземпляр класу Auth для використання
+auth_service = Auth()
 
 @app.get("/")
 def index():
-    return {'message':'PhotoShare Application'}
+    return {'message': 'PhotoShare Application'}
 
+# Додамо приклад використання класу Auth
+@app.get("/protected-route")
+async def protected_route(current_user: User = Depends(auth_service.get_current_user(role=UserRole.ADMIN))):
+    return {"message": "Welcome to the protected route, Admin!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
