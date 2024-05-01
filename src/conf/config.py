99732e6bb4db6a6df16c1fdddf1aac2e96b21472
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-05-01 12:14:25 +0300

    Add files via upload

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,24 +1,35 @@
 from typing import Any
-
+import os
 from pydantic import ConfigDict, field_validator, EmailStr
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    DB_URL: str = "postgresql+asyncpg://user:password@localhost:5432/api_wizards"
-    SECRET_KEY_JWT: str = "1234567890"
-    ALGORITHM: str = "HS256"
+    POSTGRES_DB: str = os.getenv('POSTGRES_DB')
+    POSTGRES_USER: str = os.getenv('POSTGRES_USER')
+    POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD')
+    POSTGRES_PORT: str = os.getenv('POSTGRES_PORT')
+    POSTGRES_DOMAIN: str = os.getenv('POSTGRES_DOMAIN')
+    DB_URL: str = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_DOMAIN}:{POSTGRES_PORT}/{POSTGRES_DB}"       
+    SECRET_KEY_JWT: str = os.getenv('SECRET_KEY_JWT')
+    ALGORITHM: str = os.getenv('ALGORITHM')
     MAIL_USERNAME: EmailStr = "postgres@meail.com"
-    MAIL_PASSWORD: str = "postgres"
-    MAIL_FROM: str = "postgres"
-    MAIL_PORT: int = 567234
-    MAIL_SERVER: str = "postgres"
+    MAIL_PASSWORD: str = os.getenv('MAIL_PASSWORD')
+    MAIL_FROM: str = os.getenv('MAIL_FROM')
+    MAIL_PORT: int = os.getenv('MAIL_PORT')
+    MAIL_SERVER: str = os.getenv('MAIL_SERVER')
     REDIS_DOMAIN: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_PASSWORD: str | None = None
-    # CLD_NAME: str = 
-    # CLD_API_KEY: int =
-    # CLD_API_SECRET: str = 
+    cloud_name: str = os.getenv('cloud_name')
+    api_key: str = os.getenv('api_key')
+    api_secret: str = os.getenv('api_secret')
+    
+
+
 
     @field_validator("ALGORITHM")
     @classmethod
@@ -26,9 +37,8 @@ class Settings(BaseSettings):
         if v not in ["HS256", "HS512"]:
             raise ValueError("algorithm must be HS256 or HS512")
         return v
-
-
+    
     model_config = ConfigDict(extra='ignore', env_file=".env", env_file_encoding="utf-8")  # noqa
-
+    
 
 config = Settings()
