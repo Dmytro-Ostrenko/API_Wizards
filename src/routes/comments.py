06b9112655af5f0e9d7c8bc8b4190e5cd80eff3f
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-05-02 17:05:09 +0300

    Add files via upload

--- src/routes/comments.py
+++ src/routes/comments.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-#from . import schemas, repository
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import sys
 from pathlib import Path
@@ -8,46 +7,43 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
+from src.database.models import User
 from src.schemas import schemas_comments 
-import src.repository
+from src.repository import comments
 from src.database.db import get_db
-
+from src.schemas.schemas_comments import CommentUpdateSchema
+from src.services.auth import Auth
 
 router = APIRouter(prefix='/comments', tags=["comments"])
 
 
-@router.get("/comments/{comment_id}", response_model=schemas_comments.Comment)
-def read_comment(comment_id: int, db: Session = Depends(get_db)):
-    repo = repository.CommentRepository(db)
-    db_comment = repo.get_comment_by_id(comment_id)
+@router.get("/comments/{photo_id}/comments", response_model=schemas_comments.Comment)
+async def read_comment(photo_id: int, db: AsyncSession = Depends(get_db), user: User = Depends(Auth.get_current_user)):
+    db_comment = await comments.get_comments(photo_id, db, user)
     if db_comment is None:
         raise HTTPException(status_code=404, detail="Comment not found")
     return db_comment
 
 @router.post("/comments/", response_model=schemas_comments.Comment)
-def create_comment(comment: schemas_comments.Comment, db: Session = Depends(get_db)):
-    repo = repository.CommentRepository(db)
-    return repo.create_comment(comment)
-
-@router.put("/comments/{comment_id}", response_model=schemas_comments.Comment)
-def update_comment(
-    comment_id: int, 
-    comment: schemas_comments.Comment, 
-    db: Session = Depends(get_db)
-):
-    db_comment = db.query(models.Comment).get(comment_id)
-    if not db_comment:
-        raise HTTPException(status_code=404, detail="Коментар не знайдено")
-    for key, value in comment.dict().items():
-        setattr(db_comment, key, value)
-    db.commit()
-    db.refresh(db_comment)
+async def create_comment_route(comment: schemas_comments.CommentCreate, photo_id: int, user_id: int, db: AsyncSession = Depends(get_db), user: User = Depends(Auth.get_current_user)):
+    db_comment = await comments.create_comment(comment=comment, photo_id=photo_id, user_id=user_id, db=db, user=user)
     return db_comment
 
 
+@router.put("/comments/{comment_id}")
+async def update_comment_route(comment_id: int, comment: CommentUpdateSchema, db: AsyncSession = Depends(get_db),
+                               user: User = Depends(Auth.get_current_user)):
+    if not comment.description:
+        raise HTTPException(status_code=400, detail="Description field is required")
+
+    updated_comment = await comments.update_comment(comment_id, comment, db, user)
+    if updated_comment is None:
+        raise HTTPException(status_code=404, detail="Comment not found")
+    return updated_comment
+
 @router.delete("/comments/{comment_id}")
-def delete_comment(comment_id: int, db: Session = Depends(get_db)):
-    repo = repository.CommentRepository(db)
-    if not repo.delete_comment(comment_id):
+async def delete_comment(comment_id: int, db: AsyncSession = Depends(get_db), user: User = Depends(Auth.get_current_user)):
+    deleted_comment = delete_comment(comment_id, db, user)
+    if not deleted_comment:
         raise HTTPException(status_code=404, detail="Comment not found")
     return {"message": "Comment deleted successfully"}
