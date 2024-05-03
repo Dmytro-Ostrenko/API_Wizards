d1cb6d3fbae97da3af0d510d9927a52263e25a01
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-05-03 11:06:04 +0300

    Add files via upload

--- src/schemas/schemas_comments.py
+++ src/schemas/schemas_comments.py
@@ -1,9 +1,8 @@
+ 
 from datetime import datetime
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
-
 app = FastAPI()
 
 # Модель для коментаря
@@ -14,14 +13,11 @@ class Comment(BaseModel):
     created_at: datetime
     updated_at: datetime
     description: str
-
     class Config:
         orm_mode = True
-
 class CommentCreate(BaseModel):
     description: str
     user_id: int
     photo_id: int
-
 class CommentUpdateSchema(BaseModel):
     description: str
