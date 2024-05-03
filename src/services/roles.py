30be409a25635f1bc4c1617204a65a8a9a2c6399
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-05-03 17:14:03 +0300

    Add files via upload

--- src/services/roles.py
+++ src/services/roles.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 from fastapi import Request, Depends, HTTPException, status
 
 import sys
 from pathlib import Path
+
 
 # Добавляем корневую папку проекта в sys.path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
