26f11a3640569b94e40f8232fea62f282ec5f5ae
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-05-04 14:34:26 +0300

    Add files via upload

--- src/repository/users.py
+++ src/repository/users.py
@@ -1,17 +1,19 @@
 from libgravatar import Gravatar
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
-from typing import List
+from sqlalchemy import select, func, join
+from typing import List, Union
+from fastapi import HTTPException
+
 import sys
 from pathlib import Path
 
-
+# Добавляем корневую папку проекта в sys.path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
 
-from src.database.models import User, Role
+from src.database.models import User, Role, Photos
 from src.schemas.schemas_auth import UserModel
-
+from src.schemas.user import UserResponse
 
 async def get_user_by_email(email: str, db: AsyncSession) -> User:
     stmt = select(User).filter(User.email==email)
@@ -20,30 +22,49 @@ async def get_user_by_email(email: str, db: AsyncSession) -> User:
     return user
 
 
+async def check_username_unique(username: str, db: AsyncSession) -> bool:
+    """
+    Check if the username is unique.
+
+    :param username: str: Username to check
+    :param db: AsyncSession: Async database session
+    :return: bool: True if the username is unique, False otherwise
+    """
+    user = await db.execute(select(User).filter(User.username == username))
+    return user.scalar_one_or_none() is None
+
+
 async def create_user(body: UserModel, db: AsyncSession, role: Role) -> User:
     """
     The create_user function creates a new user in the database.
-        Args:
-            body (UserModel): The UserModel object containing the data to be inserted into the database.
-            db (Session): The SQLAlchemy Session object used to interact with the database.
-        Returns:
-            User: A newly created user from the database.
+    Args:
+        body (UserModel): The UserModel object containing the data to be inserted into the database.
+        db (Session): The SQLAlchemy Session object used to interact with the database.
+    Returns:
+        User: A newly created user from the database.
 
     :param body: UserModel: Create a new user
     :param db: Session: Create a database session
     :return: A user object
+    :doc-author: Trelent
     """
+    # Перевіряємо унікальність імені користувача
+    if not await check_username_unique(body.username, db):
+        raise HTTPException(status_code=400, detail="Username is already taken")
+
     avatar = None
     try:
         g = Gravatar(body.email)
         avatar = g.get_image()
     except Exception as e:
         print(e)
+
     new_user = User(**body.dict(), avatar=avatar, role=role.value)
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
     return new_user
+
 
 
 async def update_token(user: User, token: str | None, db: AsyncSession) -> None:
@@ -54,47 +75,26 @@ async def update_token(user: User, token: str | None, db: AsyncSession) -> None:
     :param token: str | None: Update the user's refresh token
     :param db: Session: Commit the changes to the database
     :return: None
+    :doc-author: Trelent
     """
     user.refresh_token = token
     await db.commit()
 
 async def confirmed_email(email: str, db: AsyncSession) -> None:
-    """
-    The confirmed_email function marks a user as confirmed in the database.
-    
-    :param email: str: Get the email of the user
-    :param db: AsyncSession: Pass in the database session
-    :return: None
-    """
     user = await get_user_by_email(email, db)
     user.confirmed = True
     await db.commit()
 
 
 async def update_avatar_url(email: str, url: str | None, db: AsyncSession) -> User:
-    """
-    The update_avatar_url function updates the avatar url of a user.
-    
-    :param email: str: Find the user in the database
-    :param url: str | None: Specify that the url parameter can be a string or none
-    :param db: AsyncSession: Pass in the database session
-    :return: A user object
-    """
     user = await get_user_by_email(email, db)
     user.avatar = url
     await db.commit()
     await db.refresh(user)
     return user
 
+
 async def get_all_users(db: AsyncSession) -> List[User]:
-    """
-    The get_all_users function returns a list of all users in the database.
-        :return: A list of User objects.
-    
-    
-    :param db: AsyncSession: Pass the database session to the function
-    :return: A list of users, but i want to return a json object 
-    """
     stmt = select(User)
     user = await db.execute(stmt)
     user = user.scalars().all()
@@ -103,22 +103,20 @@ async def get_all_users(db: AsyncSession) -> List[User]:
    
 
 async def update_user_role(email: str, new_role: str, db: AsyncSession) -> User:
-    """
-    The update_user_role function updates the role of a user in the database.
-        Args:
-            email (str): The email address of the user to update.
-            new_role (str): The new role for this user.
-            db (AsyncSession): An async session object from SQLAlchemy's Asyncio engine, which is used to interact with our database.
-        Returns:
-            User: A User object representing the updated record in our database.
-    
-    :param email: str: Specify the email of the user to update
-    :param new_role: str: Set the new role of the user
-    :param db: AsyncSession: Pass the database connection to the function
-    :return: The updated user object
-    """
     user = await get_user_by_email(email, db)
     user.role = new_role
     await db.commit()
     await db.refresh(user)
     return user
+
+async def get_user_by_username(username: str, db: AsyncSession) -> User:
+    stmt = select(User).filter(User.username==username)
+    result = await db.execute(stmt)
+    user = result.scalar_one_or_none()
+    return user
+
+async def count_user_photos(username: str, db: AsyncSession) -> Union[int, None]:
+    stmt = select(func.count()).select_from(User.__table__.join(Photos)).where(User.username == username)
+    result = await db.execute(stmt)
+    photo_count = result.scalar_one_or_none()
+    return photo_count
