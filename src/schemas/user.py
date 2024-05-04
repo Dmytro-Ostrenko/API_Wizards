3744d40000f5ad6e0312de82d576e6c096245578
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-05-04 14:35:19 +0300

    Add files via upload

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
-
+from datetime import datetime
 import sys
 from pathlib import Path
 
+# Добавляем корневую папку проекта в sys.path
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
 from src.database.models import Role
-
+from typing import Optional
 
 class UserSchema(BaseModel):
     username: str = Field(min_length=3, max_length=50)
@@ -20,10 +21,11 @@ class UserResponse(BaseModel):
     email: EmailStr
     avatar: str | None
     role: Role
-
+    created_at: datetime
+    photos_uploaded: int| None
     model_config = ConfigDict(from_attributes = True)  # noqa
 
-
+            
 class TokenSchema(BaseModel):
     access_token: str
     refresh_token: str
