f383ed4453868535915b5bbc83d67c109812dab8
Dmytro-Ostrenko <142011911+Dmytro-Ostrenko@users.noreply.github.com>
2024-05-07 12:19:43 +0300

    Update auth.py

--- src/services/auth.py
+++ src/services/auth.py
@@ -110,5 +110,22 @@ class Auth:
         await repository_users.update_user_role(user.email, user.role, db)
 
         return {"message": "Role updated successfully"}
+    
+    def create_email_token(self, data: dict):
+        to_encode = data.copy()
+        expire = datetime.utcnow() + timedelta(days=1)
+        to_encode.update({"iat": datetime.utcnow(), "exp": expire})
+        token = jwt.encode(to_encode, self.SECRET_KEY, algorithm=self.ALGORITHM)
+        return token
+
+    async def get_email_from_token(self, token: str):
+        try:
+            payload = jwt.decode(token, self.SECRET_KEY, algorithms=[self.ALGORITHM])
+            email = payload["sub"]
+            return email
+        except JWTError as e:
+            print(e)
+            raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                                detail="Invalid token for email verification")
 
 auth_service = Auth()
